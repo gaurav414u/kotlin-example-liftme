1be6ffce7f9ec49dff2daec3c965beef5ff72930
Gaurav Bhola <gaurav414u@gmail.com>
2018-12-04 07:58:14 -0800

    Making the direction private in FloorButton

--- liftmelib/src/main/kotlin/com/example/liftmelib/FloorButton.kt
+++ liftmelib/src/main/kotlin/com/example/liftmelib/FloorButton.kt
@@ -1,6 +1,6 @@
 package com.example.liftmelib
 
-class FloorButton(floor: Int, val direction: Direction, requestHandler: RequestHandler) : Button(floor, requestHandler) {
+class FloorButton(floor: Int, private val direction: Direction, requestHandler: RequestHandler) : Button(floor, requestHandler) {
 
   override fun createRequest(): LiftRequest {
     return EnterRequest(floor, direction, this)
